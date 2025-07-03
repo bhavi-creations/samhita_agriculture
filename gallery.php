9ff79a495ad743012a79ce38996d73a64067e8c5
manimalladi <manimalladi05@gmail.com>
2025-07-03 11:36:48 +0530

    changes

--- gallery.php
+++ gallery.php
@@ -6,44 +6,79 @@
         <h1 class=" text-center p-5 ">Gallery </h1>
         <div class="row">
 
+
+            <div class="col-md-4 col-12  ">
+                <img src="assets/images/bottles/gallery_img5.png" alt="" class="Gallery_section img-fluid">
+            </div>
+
+
+
+
+            <div class="col-md-4 col-12  ">
+                <img src="assets/images/logo/g1.png" alt="" class="Gallery_section img-fluid">
+            </div>
+
+
+
+
+            <div class="col-md-4 col-12  ">
+                <img src="assets/images/bottles/M.jpg" alt="" class="Gallery_section img-fluid">
+            </div>
+
+            <div class="col-md-4 col-12  ">
+                <img src="assets/images/bottles/gallery_img7.png" alt="" class="Gallery_section img-fluid">
+            </div>
+
+
+            <div class="col-md-4 col-12  ">
+                <img src="assets/images/bottles/gallery_img8.png" alt="" class="Gallery_section img-fluid">
+            </div>
+
+
+            <div class="col-md-4 col-12  ">
+                <img src="assets/images/bottles/gallery_img10.png" alt="" class="Gallery_section img-fluid">
+            </div>
+            <div class="col-md-4 col-12  ">
+                <img src="assets/images/logo/g3.png" alt="" class="Gallery_section img-fluid">
+            </div>
+            <div class="col-md-4 col-12  ">
+                <img src="assets/images/logo/g4.png" alt="" class="Gallery_section img-fluid">
+            </div>
+
+
             <div class="col-md-4 col-12  ">
                 <img src="assets/images/logo/g11.png" alt="" class="Gallery_section img-fluid">
             </div>
 
 
-            <div class="col-md-4 col-12  ">
-                <img src="assets/images/bottles/gallery_img2.png" alt="" class="Gallery_section img-fluid">
-            </div>
-            <div class="col-md-4 col-12  ">
-                <img src="assets/images/logo/g1.png" alt="" class="Gallery_section img-fluid">
-            </div>
-            <div class="col-md-4 col-12  ">
+
+
+
+
+
+
+
+
+
+            <!-- <div class="col-md-4 col-12  ">
                 <img src="assets/images/bottles/gallery_img1.png" alt="" class="Gallery_section img-fluid">
             </div>
 
-            <!-- <div class="col-md-4 col-12  ">
+
+
+          
+
+
+            <div class="col-md-4 col-12  ">
                 <img src="assets/images/bottles/gallery_img3.png" alt="" class="Gallery_section img-fluid">
             </div>
-            <div class="col-md-4 col-12  ">
-                <img src="assets/images/bottles/gallery_img4.png" alt="" class="Gallery_section img-fluid">
-            </div> 
-            <div class="col-md-4 col-12  ">
-                <img src="assets/images/logo/g3.png" alt="" class="Gallery_section img-fluid">
-            </div>-->
-            <div class="col-md-4 col-12  ">
-                <img src="assets/images/logo/g4.png" alt="" class="Gallery_section img-fluid">
-            </div>
-            <!-- <div class="col-md-4 col-12  ">
+
+
+
+
+
+              <div class="col-md-4 col-12  ">
                 <img src="assets/images/logo/g5.png" alt="" class="Gallery_section img-fluid">
-            </div>
-            <div class="col-md-4 col-12  ">
-                <img src="assets/images/logo/g6.png" alt="" class="Gallery_section img-fluid">
-            </div>
-            <div class="col-md-4 col-12  ">
-                <img src="assets/images/logo/g7.png" alt="" class="Gallery_section img-fluid">
-            </div> -->
-            <!-- <div class="col-md-4 col-12  ">
-                <img src="assets/images/logo/g8.png" alt="" class="Gallery_section img-fluid">
             </div> -->
 
         </div>
